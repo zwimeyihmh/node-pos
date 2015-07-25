2dda5d35c6ce71ae42c5eb012776c376e4055d59
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-25 22:42:24 +0800

    add tags to main.js

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 var Pos = require('./model/pos.js');
-var pos = new Pos();
+var Scan = require('./model/scan.js');
+var Cart = require('./model/cart.js');
+
+var tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'
+    ];
+var scan = new Scan();
+var cart = new Cart();
+var pos = new Pos(scan,cart);
 console.log(pos.printReceipt());
