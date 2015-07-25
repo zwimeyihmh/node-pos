d3232543fb2683a88c81683c2d18a8d7e8e60121
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-25 22:51:58 +0800

    add scan() to pos.js

--- model/pos.js
+++ model/pos.js
@@ -1,12 +1,21 @@
+var Cart = require('./cart.js');
 var Receipt = require('./receipt');
 function Pos(scanner,cart){
   this.scaanner = scanner;
-  this.cart = cart ;
+  this.cart = cart || new Cart() ;
+}
+
+Pos.prototype.scan = function(tags){
+  for (var i = 0; i < tags.length; i++)
+  {
+    cartItem = this.scanner.scan(tags[i]);
+    this.cart.addCartItem(cartItem);
+  }
 }
 
 Pos.prototype.printReceipt = function(){
   var receipt = new Receipt();
-  return receipt.printed();
+  return receipt.printed(this.cart);
 }
 
 module.exports = Pos;
