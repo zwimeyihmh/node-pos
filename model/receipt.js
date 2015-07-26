9da1017ad8b74bfd853e56f17f9a0cfb5be5258f
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-26 15:39:14 +0800

    add something want to print

--- model/receipt.js
+++ model/receipt.js
@@ -1,21 +1,23 @@
 var Utilities = require('./utilities.js');
-//var Cart = require('./cart.js');
+var PriceDifference = require('./price-difference.js');
 function Receipt(){
 
 }
 Receipt.prototype.printed = function(cart){
   var utilities= new Utilities();
-//  var promotionPrice = new PriceDifference();
+  cart.getPromotions();
+  var promotionPrice = new PriceDifference();
   return ('***<没钱赚商店>收据***\n' +
   '打印时间：' + utilities.getTime() + '\n' +
   '----------------------\n' +
-//  cart.getItemsString() +
+ cart.getItemsString() +
+//  cart.print() +
   '----------------------\n' +
   '挥泪赠送商品：\n' +
-  //cart.getPromotionsString() +
+  cart.getPromotionsString() +
   '----------------------\n' +
-  //'总计：' + utilities.formatPrice(cart.getAmount()) + '(元)\n' +
-  //'节省：' + utilities.formatPrice(promotionPrice.getPromotedAmount(cart.promotions)) + '(元)\n' +
+  '总计：' + utilities.formatPrice(cart.getAmount()) + '(元)\n' +
+  '节省：' + utilities.formatPrice(promotionPrice.getPromotedAmount(cart.promotions)) + '(元)\n' +
     '**********************');
-}
+};
 module.exports = Receipt;
