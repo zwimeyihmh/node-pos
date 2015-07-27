eb045cf1c17b77c4e44ed4dddb90adcc7f595165
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-27 15:15:15 +0800

    add

--- model/scan.js
+++ model/scan.js
@@ -1,17 +1,20 @@
-var CartItem = require('./cart_item.js');
+var CartItem = require('./cart-item.js');
+
 function Scan(tag) {
   this.tag = tag || '';
 }
 
 Scan.prototype.scan = function(tag) {
   var count = 1;
+  var cartitem = new CartItem();
+
   var ifExist = tag.indexOf('-');
   if (ifExist !== -1) {
     var arrayTag = tag.split('-');
     count = parseFloat(arrayTag[1]);
     tag = arrayTag[0];
   }
-  var cartitem = new CartItem();
+
   return ({
     item: cartitem.findItem(tag),
     count: count
