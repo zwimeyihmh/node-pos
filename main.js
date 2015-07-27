38c927a2d5d20fc800f5a96b3e3215d231599481
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-27 15:05:12 +0800

    standard

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ var Pos = require('./model/pos.js');
 var Scan = require('./model/scan.js');
 var Cart = require('./model/cart.js');
 
-var tags = [
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000001',
-      'ITEM000003-2',
-      'ITEM000005',
-      'ITEM000005',
-      'ITEM000005'
-    ];
-var scan = new Scan();
-var cart = new Cart();
-var pos = new Pos(scan,cart);
-console.log(pos.printReceipt());
+function printReceipt(tags) {
+  var scan = new Scan();
+  var cart = new Cart();
+  var pos = new Pos(scan, cart);
+
+  pos.scan(tags);
+  console.log(pos.print());
+}
+
+exports.printReceipt = printReceipt;
--- model/pos.js
+++ model/pos.js
@@ -2,23 +2,20 @@ var Cart = require('./cart.js');
 var Receipt = require('./receipt.js');
 var Scan = require('./scan.js');
 
-
-function Pos(scanner,cart){
+function Pos(scanner, cart) {
   this.scanner = scanner || new Scan();
-  this.cart = cart || new Cart() ;
+  this.cart = cart || new Cart();
 }
 
-Pos.prototype.scan = function(tags){
-  for (var i = 0; i < tags.length; i++){
+Pos.prototype.scan = function(tags) {
+  for (var i = 0; i < tags.length; i++) {
     cartItem = this.scanner.scan(tags[i]);
     this.cart.addCartItem(cartItem);
-    }
+  }
 };
 
-Pos.prototype.printReceipt = function() {
+Pos.prototype.print = function() {
   var receipt = new Receipt();
-  //this.cart.getPromotions();
-//  console.log(this.cart.promotions);
   return receipt.printed(this.cart);
 };
 
