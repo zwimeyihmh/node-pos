e6c7caf4c27030f8039ed6f573443133a0a5409a
zwimeyihmh <zwimeyihmh@outlook.com>
2015-07-27 14:54:48 +0800

    move getItemsString() and getPromotionsString() from  Cart to Receipt

--- model/cart.js
+++ model/cart.js
@@ -3,13 +3,12 @@ module.exports = Cart;
 var Utilities = require('./utilities.js');
 var CartItem = require('./cart-item.js');
 var PromotedItem = require('./promoted-item.js');
-var PriceDifference = require('./price-difference.js');
+var Discounter = require('./discounter.js');
 var Promotion = require('./promotion.js');
 var fixtures = require('../fixtures.js');
 
 function Cart() {
   this.cartItems = [];
-  this.promotions = [];
 }
 
 Cart.prototype.addCartItem = function(cartItem) {
@@ -27,84 +26,13 @@ Cart.findCartItem = function(cartItem,cartItems) {
       return cartItems[i];
     }
   }
-//  return undefined;
 };
 
-Cart.prototype.getPromotions = function() {
-  var promotion;
-  for (var i = 0; i < this.cartItems.length; i++) {
-    promotion = this.promoteItems(this.cartItems[i]);
-    //  this.promotions.push(promotion)
-    if (promotion) {
-      (this.promotions).push(promotion);
-    }
-  }
-
-};
-
-
-
-
-
-Cart.prototype.promoteItems = function(item) {
-  var promotionsLoad = fixtures.loadPromotions();
-  var promotion;
-  var promotionsBarcode = promotionsLoad[0].barcodes;
-  promotionsBarcode.forEach(function(promotionBarcode) {
-    if (item.item.barcode === promotionBarcode) {
-      promotion = {
-        item: item.item,
-        count: Math.floor(item.count / 3)
-      };
-    }
-  });
-  return promotion;
-};
-
-Cart.prototype.getItemsString = function() {
-  var itemsString = '';
-  var utilities = new Utilities();
-  var cartItem = new CartItem();
-  for (var i = 0; i < this.cartItems.length; i++) {
-    var subtotal = this.getSubTotal(this.cartItems[i]);
-    itemsString += cartItem.getString(this.cartItems[i]) + '小计：' + utilities.formatPrice(subtotal) + '(元)\n';
-  }
-
-  return itemsString;
-};
-
-
-Cart.prototype.getPromotionsString = function() {
-  //this.getPromotions();
-  var promotionsSting = '';
-  var promotionItem = new PromotedItem();
-  this.promotions.forEach(function(promotion) {
-    promotionsSting += promotionItem.getString(promotion);
-  });
-  return promotionsSting;
-};
-
-Cart.prototype.getSubTotal = function(item) {
-  var cartItem = new CartItem();
-
-  var subtotal = cartItem.subTotal(item.item.price, item.count);
-  var priceDifference = new PriceDifference();
-  if (priceDifference.getSubPrice(item)) {
-    subtotal = priceDifference.getSubPrice(item);
-  }
-  return subtotal;
-};
-
-Cart.prototype.print = function() {
-//  console.log(this.cartItems);
-};
 Cart.prototype.getAmount = function() {
   var amount = 0;
-  var priceDifference = new PriceDifference();
   var cartItem = new CartItem();
   for (var i = 0; i < this.cartItems.length; i++) {
-    amount += this.getSubTotal(this.cartItems[i]);
+    amount += cartItem.getSubTotal(this.cartItems[i]);
   }
   return amount;
 };
-//module.exports = Cart;
--- model/receipt.js
+++ model/receipt.js
@@ -1,23 +1,46 @@
 var Utilities = require('./utilities.js');
-var PriceDifference = require('./price-difference.js');
+var Discounter = require('./discounter.js');
+var CartItem = require('./cart-item.js');
+var PromotedItem = require('./promoted-item.js');
 function Receipt(){
 
 }
 Receipt.prototype.printed = function(cart){
   var utilities= new Utilities();
-  cart.getPromotions();
-  var promotionPrice = new PriceDifference();
+  var discounter = new Discounter();
+  discounter.getPromotions(cart.cartItems);
   return ('***<没钱赚商店>收据***\n' +
   '打印时间：' + utilities.getTime() + '\n' +
   '----------------------\n' +
- cart.getItemsString() +
-//  cart.print() +
+ this.getItemsString(cart) +
   '----------------------\n' +
   '挥泪赠送商品：\n' +
-  cart.getPromotionsString() +
+  this.getPromotionsString(discounter.promotions) +
   '----------------------\n' +
   '总计：' + utilities.formatPrice(cart.getAmount()) + '(元)\n' +
-  '节省：' + utilities.formatPrice(promotionPrice.getPromotedAmount(cart.promotions)) + '(元)\n' +
+  '节省：' + utilities.formatPrice(discounter.getPromotedAmount(discounter.promotions)) + '(元)\n' +
     '**********************');
 };
+
+Receipt.prototype.getItemsString = function(cart) {
+  var itemsString = '';
+  var utilities = new Utilities();
+  var cartItem = new CartItem();
+  for (var i = 0; i < cart.cartItems.length; i++) {
+    var subtotal = cartItem.getSubTotal(cart.cartItems[i]);
+    itemsString += cartItem.getString(cart.cartItems[i]) + '小计：' + utilities.formatPrice(subtotal) + '(元)\n';
+  }
+
+  return itemsString;
+};
+
+Receipt.prototype.getPromotionsString = function(promotions) {
+  var promotionsSting = '';
+  var promotionItem = new PromotedItem();
+  promotions.forEach(function(promotion) {
+    promotionsSting += promotionItem.getString(promotion);
+  });
+  return promotionsSting;
+};
+
 module.exports = Receipt;
