96e993da6b02a0d6a6d122be83235541d46e68d7
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-05 00:21:44 +0800

    add test for receiptItem

--- spec/receipt-item-spec.js
+++ spec/receipt-item-spec.js
@@ -8,10 +8,10 @@ describe('ReceiptItem', function() {
         item:new Item('ITEM000001', '雪碧', '瓶', 3.00),
         count:5
       };
-      var string = '名称：雪碧，数量：5瓶，单价：3.00(元),';
+      var string = '名称：雪碧，数量：5瓶，单价：3.00(元)，';
       var receiptItem = new ReceiptItem();
       var result = receiptItem.getString(cartItem);
-      expect(result).toBe(string);
+      expect(result).toEqual(string);
     });
 
   });
@@ -37,9 +37,9 @@ describe('ReceiptItem', function() {
         item:new Item('ITEM000001', '雪碧', '瓶', 3.00),
         count:1
       };
-      var string = '名称：雪碧，数量：1瓶';
-      expect(receiptItem.getPromotionString(promotion)).toBe(string);
-    })
+      var string = '名称：雪碧，数量：1瓶\n';
+      expect(receiptItem.getPromotionString(promotion)).toEqual(string);
+    });
 
   });
 
