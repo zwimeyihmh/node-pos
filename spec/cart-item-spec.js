8084149ebc83378cb0cbfb3590c086fab45a385f
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-05 00:20:19 +0800

    add test for cartItem

--- spec/cart-item-spec.js
+++ spec/cart-item-spec.js
@@ -2,18 +2,22 @@ var CartItem = require('../model/cart-item.js');
 var Item = require('../model/item.js');
 
 describe('CartItem',function() {
+
   describe('#findCartItem()',function() {
+
     it('can return cartItem',function() {
       var cartItem = new CartItem() ;
       var result = cartItem.findItem('ITEM000001');
       var item = new Item('ITEM000001', '雪碧', '瓶', 3.00);
       expect(result).toEqual(item);
     });
+
     it('can return undefined',function() {
       var cartItem = new CartItem();
       var result = cartItem.findItem('ITEM000009');
       expect(result).toBe(undefined);
-    })
+    });
+
   });
 
 });
