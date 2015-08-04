dceacc0e1fa27bf0b0e9c771f47553cdc899d23e
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-05 00:19:39 +0800

    add test for scan tag

--- spec/scan-spec.js
+++ spec/scan-spec.js
@@ -1,5 +1,5 @@
 var Scan = require('../model/scan.js');
-var CartItem = require('../model/cart-item.js');
+var Item = require('../model/item.js');
 
 describe('Scan',function() {
 
@@ -8,8 +8,8 @@ describe('Scan',function() {
     it('cartItem',function() {
 
       var scan = new Scan();
-      var cartItem = new CartItem();
-      expect(scan.scan('ITEM000001')).toBe({item:cartItem.findItem('ITEM000001'),count:1});
+
+      expect(scan.scan('ITEM000001')).toEqual({item:new Item('ITEM000001', '雪碧', '瓶', 3.00),count:1});
 
     });
 
