f03bdf15529c8650f31d3ef862baa5c20253e43b
zwimeyihmh <zwimeyihmh@outlook.com>
2015-08-05 00:24:34 +0800

    change of pos

--- model/pos.js
+++ model/pos.js
@@ -8,10 +8,12 @@ function Pos(scanner, cart) {
 }
 
 Pos.prototype.scan = function(tags) {
+
   for (var i = 0; i < tags.length; i++) {
     cartItem = this.scanner.scan(tags[i]);
     this.cart.addCartItem(cartItem);
   }
+
 };
 
 Pos.prototype.print = function() {
